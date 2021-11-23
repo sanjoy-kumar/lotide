7fed49657e181b02f8884c58a91b8a0e16ed1705
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2021-11-23 18:53:39 -0500

    head function added.

--- assertEqual.js
+++ assertEqual.js
@@ -3,10 +3,12 @@ const assertEqual = function(actual, expected) {
   var emojiPass = String.fromCodePoint(0x2705);
   var emojiFail = String.fromCodePoint(0x274C);
   if(actual === expected){
-    console.log(emojiPass + emojiPass + emojiPass+ "Assertion Passed: " + [actual] + "===" + [expected]);
+    //console.log(emojiPass + emojiPass + emojiPass+ "Assertion Passed: " + [actual] + "===" + [expected]);
+    console.log(`${emojiPass}${emojiPass}${emojiPass} Assertion Passed: ${actual} === ${expected}`);
   } else
   {
-    console.log(emojiFail+emojiFail+emojiFail+ "Assertion Failed: " + [actual] + "!==" + [expected]);
+    //console.log(emojiFail+emojiFail+emojiFail+ "Assertion Failed: " + [actual] + "!==" + [expected]);
+    console.log(`${emojiFail}${emojiFail}${emojiFail} Assertion Passed: ${actual} !== ${expected}`);
   } 
 
 };
@@ -14,4 +16,10 @@ const assertEqual = function(actual, expected) {
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+
+
+//The next example greets Alice using the template literals syntax.
+
+const name = 'Alice';
+console.log(`Hello, ${name}!`); // logs: Hello, Alice!
 
