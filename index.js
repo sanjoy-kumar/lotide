249c48187c59adaf7dc3f68fa45c79069f0bbe73
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2021-11-30 00:36:34 -0500

    Implement Object Shorthand

--- index.js
+++ index.js
@@ -3,9 +3,5 @@ const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
 
-module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle
-};
+module.exports = {head, tail, middle};
 
